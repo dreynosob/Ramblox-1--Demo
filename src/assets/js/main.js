521748cf0d79869dda8e27d3b3eb4a9a9a5ce15a
Darlyn <darlynreynoso74@gmail.com>
2022-05-04 12:29:17 +0200

    cambiada function (main.js) on-top

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -368,7 +368,7 @@
 		});  
 		// Click Event
 		$('.go-top').on('click', function() {
-			$("html, body").animate({ scrollTop: "0" },  500);
+			$("html, body").animate({ scrollTop: "0" },  100);
 		});
 	});
 
